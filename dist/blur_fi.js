ff97e674e72185fce156db8bcde1dfa2e716ee80
Marcus Tallberg <marcus.tallberg@gmail.com>
2019-04-11 14:31:08 +0300

    Hg-version of repo imported from Bitbucket

---
+++ dist/blur_fi.js
@@ -0,0 +1,33 @@
+/*! Blur Fi - v0.1.0 - 2013-06-12
+* https://github.com/marcus/blur_fi
+* Copyright (c) 2013 Marcus Tallberg; Licensed MIT */
+(function($) {
+
+  // Collection method.
+  $.fn.awesome = function() {
+    return this.each(function(i) {
+      // Do something awesome to each selected element.
+      $(this).html('awesome' + i);
+    });
+  };
+
+  // Static method.
+  $.awesome = function(options) {
+    // Override default options with passed-in options.
+    options = $.extend({}, $.awesome.options, options);
+    // Return something awesome.
+    return 'awesome' + options.punctuation;
+  };
+
+  // Static method default options.
+  $.awesome.options = {
+    punctuation: '.'
+  };
+
+  // Custom selector.
+  $.expr[':'].awesome = function(elem) {
+    // Is this element awesome?
+    return $(elem).text().indexOf('awesome') !== -1;
+  };
+
+}(jQuery));
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
-const path = require('path');
+const path = require('path')
 
- module.exports = {
-     entry: './src/js/main.js',
-     output: {
-         path: path.resolve(__dirname, 'dist'),
-         filename: 'blur.bundle.js'
-     },
-     module: {
+module.exports = {
+    entry: './src/js/main.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'blur.bundle.js'
+    },
+    module: {
         loaders: [{
             test: /\.js$/,
             exclude: /node_modules/,
             loader: 'babel-loader'
-         }]
-     }
- };
+        }]
+    }
+};
