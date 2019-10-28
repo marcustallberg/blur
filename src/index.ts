24b4094123b6933cfd46ccb7f590b2fba08939b2
Marcus Tallberg <marcus.tallberg@gmail.com>
2019-10-28 11:22:21 +0200

    Added some palettes and dropdowns for the palettes

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,27 @@ import {blur, BlurSource} from './modules/blur'
 const canvas = <HTMLCanvasElement>document.getElementById('blurCanvas')
 const ctx = <CanvasRenderingContext2D>canvas.getContext('2d')
 
+const changeHandler = () => {
+  const imageData = ctx.getImageData(0, 0, img.width, img.height)
+  const source: BlurSource = {
+    imageData,
+    pixelSize: Number(pixelsize.value),
+    palette: palette.value
+  }
+  blur(source) 
+}
+
+const palette = <HTMLSelectElement>document.getElementById("palette")
+palette.addEventListener('change', changeHandler)
+
+const pixelsize = <HTMLSelectElement>document.getElementById("pixelsize")
+pixelsize.addEventListener('change', changeHandler)
+
 const img = new Image()
 
 img.addEventListener('load', () => {
   ctx.drawImage(img, 0, 0)
-  const imageData = ctx.getImageData(0, 0, img.width, img.height)
-  const source: BlurSource = {
-    imageData,
-    pixelSize: 10
-  }
-
-  blur(source) 
-
-
+  changeHandler()
 
 }, false)
 
